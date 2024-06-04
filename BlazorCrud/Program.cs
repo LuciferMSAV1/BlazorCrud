dec7478dbeaab6a4bf602a0d9a0bec51e49e91ed
gangs <gangs@DESKTOP-FETTB1F>
2024-06-04 15:59:50 +0500

    Add project files.

---
+++ BlazorCrud/Program.cs
@@ -0,0 +1,42 @@
+using BlazorCrud.Data;
+using BlazorCrud.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+
+// Add DbContext using the configured connection string
+builder.Services.AddDbContext<DatabaseContext>(options =>
+    options.UseSqlServer(configuration.GetConnectionString("Defaultaconnection")));
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
